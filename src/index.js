690e72c8a3ddcbb74e707b09986554741ee1dabf
sako-donuts-bkk <Sakoa@donuts-bkk.com>
2020-05-26 07:25:06 +0700

    fix about ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App';
 import './index.css';
 import reducer from './reducers';
 import App from './components/App';
 
 const store = createStore(reducer);
-ReactDOM.render(<App />,
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
